34717dd434791d336c9564401e57f381bf751133
AlienNT <nick.tristen22@gmail.com>
2023-04-18 18:43:13 +0300

    add profile component

---
+++ src/components/pages/VProfilePage.vue
@@ -0,0 +1,16 @@
+<template>
+<div class="v-profile-page">
+  <div class="profile-image"></div>
+  <div class="profile-info"></div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "VProfilePage"
+}
+</script>
+
+<style scoped>
+
+</style>
